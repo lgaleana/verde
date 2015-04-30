4bd9af8e078e28328789eefc0b56db9c96638806
Luis Galeana <luis.galeana@oracle.com>
2015-04-30 13:29:56 -0700

    Fixed map and added setup activity

--- app/src/main/java/com/oracle/verde/SetupActivity.java
+++ app/src/main/java/com/oracle/verde/SetupActivity.java
@@ -1,39 +1,24 @@
 package com.oracle.verde;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.Button;
+import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+
+public class SetupActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        WebView webView = (WebView) findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/index.html");
-
-        Button addButton = (Button) findViewById(R.id.add_btn);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SetupActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_setup);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_setup, menu);
         return true;
     }
 
